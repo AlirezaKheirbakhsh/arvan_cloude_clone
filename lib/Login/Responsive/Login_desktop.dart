2a7d8909c8d4bacb9acc5b7d53fb52b20670e6e6
areza7551 <areza7551@gmail.com>
2023-06-08 14:23:31 +0330

    make login contoner

--- lib/Login/Responsive/Login_desktop.dart
+++ lib/Login/Responsive/Login_desktop.dart
@@ -160,7 +160,7 @@ class _Login_desktopState extends State<Login_desktop> {
                         height: 650,
                         decoration: const BoxDecoration(
                           borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: const Color.fromARGB(255, 255, 255, 255),
+                          color: Color.fromARGB(255, 255, 255, 255),
                           boxShadow: [
                             BoxShadow(
                               color: Color.fromARGB(10, 0, 0, 0),
@@ -172,6 +172,81 @@ class _Login_desktopState extends State<Login_desktop> {
                               ),
                             )
                           ],
+                        ),
+                        child: Container(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  
+                                  width: 300,
+                                  height: 50,
+                                  // color: Colors.amberAccent,
+                                  margin: const EdgeInsets.only(top: 40,bottom: 0),
+                                  child: Center(child: const Text("ورود", style: TextStyle(color: Color.fromARGB(255, 0, 186, 186),fontSize: 40,fontWeight: FontWeight.w800),)),
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 40,
+                                  margin: const EdgeInsets.only(top: 60,bottom: 0),
+
+                                  // color: const Color.fromARGB(255, 134, 255, 64),
+                                  child: TextField(
+                                  
+                                  decoration: const InputDecoration(
+                                   enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 3,color: Colors.red) ), 
+                                  border: UnderlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                                  labelText: 'Enter your username'
+
+                                  ),
+                                )
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 40,
+                                  margin: const EdgeInsets.only(top: 10,bottom: 0),
+                                  color: const Color.fromARGB(255, 64, 255, 255),
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 40,
+                                  margin: const EdgeInsets.only(top: 10,bottom: 0),
+                                  color: const Color.fromARGB(255, 64, 169, 255),
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 40,
+                                  margin: const EdgeInsets.only(top: 5,bottom: 0),
+                                  color: const Color.fromARGB(255, 64, 83, 255),
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 40,
+                                  margin: const EdgeInsets.only(top: 60,bottom: 0),
+                                  color: const Color.fromARGB(255, 137, 64, 255),
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 40,
+                                  margin: const EdgeInsets.only(top: 10,bottom: 0),
+                                  color: const Color.fromARGB(255, 255, 64, 239),
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 40,
+                                  margin: const EdgeInsets.only(top: 20,bottom: 20),
+                                  color: Color.fromARGB(255, 255, 64, 128),
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 40,
+                                  margin: const EdgeInsets.only(top: 0,bottom: 0),
+                                  color: Color.fromARGB(255, 255, 0, 0),
+                                ),
+                                
+                              ],
+                        
+                          ),
                         ),
                       ),
                     ],
